46edcd74742402cb92ac5aa36bf6aa0247a07d59
Alejandro Juan Garcia <44984326+alexcorvis84@users.noreply.github.com>
2018-11-18 17:31:36 +0100

    Add files via upload

---
+++ ejemplos/MicroPython/src/semaforo_NeoPixel16LED.py
@@ -0,0 +1,60 @@
+import time
+
+def demo(np):
+    n = np.n
+
+    """# cycle
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (0, 0, 0)
+        np[i % n] = (255, 255, 255)
+        np.write()
+        time.sleep_ms(250)
+
+    # bounce
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (0, 0, 128)
+        if (i // n) % 2 == 0:
+            np[i % n] = (0, 0, 0)
+        else:
+            np[n - 1 - (i % n)] = (0, 0, 0)
+        np.write()
+        time.sleep_ms(60)
+
+    # fade in/out
+    for i in range(0, 4 * 256, 8):
+        for j in range(n):
+            if (i // 256) % 2 == 0:
+                val = i & 0xff
+            else:
+                val = 255 - (i & 0xff)
+            np[j] = (val, 0, 0)
+        np.write()"""
+  # clear
+    for i in range(n):
+        np[i] = (0, 0, 0)
+    np.write()
+    time.sleep_ms(5000)
+    
+    for i in range(n):
+        np[i] = (128, 0, 0)
+        time.sleep_ms(50)
+    np.write()
+    for i in range(n):
+        np[i] = (128, 128, 0)
+        time.sleep_ms(50)
+    np.write()
+    for i in range(n):
+        np[i] = (0, 128, 0)
+        time.sleep_ms(50)
+    np.write()
+    for i in range(n):
+        np[i] = (0, 0, 0)
+    np.write()
+        
+        
+    """# clear
+    for i in range(n):
+        np[i] = (0, 0, 0)
+    np.write()"""
